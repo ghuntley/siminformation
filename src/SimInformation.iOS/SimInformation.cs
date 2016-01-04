4c6181ca5a12cae9115f69ecb9458bf94bd2495c
Geoffrey Huntley <ghuntley@ghuntley.com>
2016-01-04 21:22:09 +1100

    added xamarin.ios project

---
+++ src/SimInformation.iOS/SimInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimInformation.iOS
+{
+    public class SimInformation : ISimInformation
+    {
+        public IReadOnlyList<SimCard> GetSimCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
